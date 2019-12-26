1f24c7a9b5d421bc7452f855b65d3cdf1dc86827
Paulo Vaz <psgvaz@gmail.com>
2019-12-26 17:04:03 -0300

    Menu - Alterado menu

--- public/js/cliente/new.js
+++ public/js/cliente/new.js
@@ -8,31 +8,11 @@ $(document).ready(function () {
         var cpf_cnpj = document.getElementById("cpf_cnpj");
         cpf_cnpj  = cpf_cnpj.value;
 
-        //console.log($('meta[name="csrf-token"]').attr('content'))
+        console.log($('input[name="_token"]').attr('content'))
 
         //Necessario para que o ajax envie o csrf-token
         //Para isso coloquei no form <meta name="csrf-token" content="{{ csrf_token() }}">
-        $.ajaxSetup({
-            headers: {
-                'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            }
-        });
 
-        var dados = {
-            'cpf_cnpj': cpf_cnpj
-        }
-
-        jQuery.ajax({
-            type: 'POST',
-            data: dados,
-            url: '/index.php/consultaCpfCnpf'
-        }).done(function (retorno) {
-
-            if(retorno.success) {
-                swal(retorno.msg, "Click no botão abaixo!", "error");
-                $("input#cpf_cnpj").val( "" );
-            }
-        });
 
     })
 
