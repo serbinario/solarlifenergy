8b81e12d4845f918cf2b9402b1e98476a376bbe5
Paulo Vaz <psgvaz@gmail.com>
2020-04-23 15:10:13 -0300

    adicionando coluna update_at

--- app/Http/Controllers/Projetov2Controller.php
+++ app/Http/Controllers/Projetov2Controller.php
@@ -67,7 +67,7 @@ class Projetov2Controller extends Controller
             ->leftJoin('projetos_prioridades', 'projetos_prioridades.id', '=', 'projetosv2.projeto_prioridade_id')
             ->select([
                 'projetosv2.id',
-                'projetov2.updated_at',
+                'projetosv2.updated_at',
                 'users.name as integrador',
                 'franquias.nome as franquaia',
                 'clientes.nome_empresa',
