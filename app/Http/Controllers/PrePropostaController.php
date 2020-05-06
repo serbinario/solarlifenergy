96219da4d75589c22ebf6caec595f421be5f9dd7
Paulo Vaz <psgvaz@gmail.com>
2020-05-06 16:21:46 -0300

    C

--- app/Http/Controllers/PrePropostaController.php
+++ app/Http/Controllers/PrePropostaController.php
@@ -281,7 +281,7 @@ class PrePropostaController extends Controller
     public function update($id, PrePropostaFormRequest $request)
     {
         try {
-            dd($request->all());
+            //dd($request->all());
             $data = $this->getData($request);
 
             $cidade = Cidade::where('id', '=', $data['cidade_id'])->first();
