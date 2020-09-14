e48b36135d02e12e1c52da0b85dc21235fc43d48
Paulo Vaz <psgvaz@gmail.com>
2020-09-14 11:35:49 -0300

    Propostas

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -30,27 +30,28 @@ class PedidoController extends Controller
      */
     public function index(Request $request)
     {
-        $pedido = Pedido::find(1);
-        $pedido->produtos()->attach([1 => [ "quantidade" => 340, "valor_unitario" => 450.1  ], 2 => [  "quantidade" => 100, "valor_unitario" => 10.1 ]]);
-        dd("qq");
-
-
-        //$sync_data = [];
-        //for($i = 0; $i < count($allergy_ids); $i++))
-        //$sync_data[$allergy_ids[$i]] = ['severity' => $severities[$i]];
-
-        //$food->allergies()->sync($sync_data);
-
-
-        //$food = Food::find(1);
-        //$food->allergies()->sync([1 => ['severity' => 3], 4 => ['severity' => 1]]);
-
-        $pedido = Pedido::find($request[0]['pedido_id']);
-        $pedido->produtos()->attach('1');
-        dd($pedido);
-        //$shop->products()->attach($product_id);
-        return dd($request->all());
-        return dd(Pedido::with('produtos')->get());
+        return view('vendas.index');
+//        $pedido = Pedido::find(1);
+//        $pedido->produtos()->attach([1 => [ "quantidade" => 340, "valor_unitario" => 450.1  ], 2 => [  "quantidade" => 100, "valor_unitario" => 10.1 ]]);
+//        dd("qq");
+//
+//
+//        //$sync_data = [];
+//        //for($i = 0; $i < count($allergy_ids); $i++))
+//        //$sync_data[$allergy_ids[$i]] = ['severity' => $severities[$i]];
+//
+//        //$food->allergies()->sync($sync_data);
+//
+//
+//        //$food = Food::find(1);
+//        //$food->allergies()->sync([1 => ['severity' => 3], 4 => ['severity' => 1]]);
+//
+//        $pedido = Pedido::find($request[0]['pedido_id']);
+//        $pedido->produtos()->attach('1');
+//        dd($pedido);
+//        //$shop->products()->attach($product_id);
+//        return dd($request->all());
+//        return dd(Pedido::with('produtos')->get());
     }
 
     /**
