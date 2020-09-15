e7021f1966f1f27fa0b9c9d3bbd5b53245086db3
Paulo Vaz <psgvaz@gmail.com>
2020-09-15 12:10:46 -0300

    Pedido autenticação

--- app/Http/Controllers/PedidoController.php
+++ app/Http/Controllers/PedidoController.php
@@ -20,7 +20,7 @@ class PedidoController extends Controller
      */
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth');
     }
 
     /**
