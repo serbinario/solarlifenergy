3e298a8a21dabec7bc977a2db49bb0cc24e1a135
Paulo Vaz <psgvaz@gmail.com>
2020-09-16 10:54:46 -0300

    Vendas

--- resources/views/vendas/finalizar.blade.php
+++ resources/views/vendas/finalizar.blade.php
@@ -1,38 +1,12 @@
-<table class="table table-hover">
+<table  class="table table-hover">
     <thead>
     <tr>
         <th>Quantidade</th>
         <th>Produto</th>
-        <th>Valor</th>
-        <th></th>
+        <th>Valor unitário</th>
+        <th>Valor Total</th>
     </tr>
     </thead>
-    <tbody>
-    <tr>
-        <td><input style="width: 54px;" type="number" value="1"/></td>
-        <td>INVERSOR KSTAR KSG 15K</td>
-        <td>R$ 15.200,00</td>
-        <td class="text-right">
-            <button type="button" class="btn btn-succes"  data-placement="top" data-original-title="Edit row"><i class="glyphicon glyphicon-plus"></i></button>
-        </td>
-    </tr>
-    <tr>
-        <td><input style="width: 54px;" type="number" value="1"/></td>
-        <td>INVERSOR KSTAR KSG 30K</td>
-        <td>R$ 26.400,00</td>
-        <td class="text-right">
-            <button type="button" class="btn btn-succes" data-placement="top" data-original-title="Edit row"><i class="glyphicon glyphicon-plus"></i></button>
-
-        </td>
-    </tr>
-    <tr>
-        <td><input style="width: 54px;" type="number" value="1"/></td>
-        <td>INVERSOR KSTAR KSG 5K</td>
-        <td>R$ 15.000,00</td>
-        <td class="text-right">
-            <button type="button" class="btn btn-succes"  data-placement="top" data-original-title="Edit row"><i class="glyphicon glyphicon-plus"></i></button>
-        </td>
-    </tr>
-
+    <tbody id="table_finalizar">
     </tbody>
 </table>
