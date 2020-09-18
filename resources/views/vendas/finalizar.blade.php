94805a44f5abd9598183bfab84f78b026d240f30
Paulo Vaz <psgvaz@gmail.com>
2020-09-18 12:19:30 -0300

    Vendas

--- resources/views/vendas/finalizar.blade.php
+++ resources/views/vendas/finalizar.blade.php
@@ -17,7 +17,7 @@
             <span class="sc-fEVUGC title">Formas de Pagamento</span>
             <ul>
                 <li>
-                    <p class="sc-dchYKM gEfkdM">Até 60x financiamento Santander, BV e Losango</p>
+                    <p class="sc-dchYKM gEfkdM">Até 60x financiamento Santander, BV</p>
                 </li>
                 <li>
                     <p class="sc-dchYKM gEfkdM">12x no cartão de crédito</p>
@@ -33,7 +33,7 @@
         <div>
             <span class="sc-iNovjJ title">Frete</span>
             <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="angle-right" class="svg-inline--fa fa-angle-right fa-w-8 " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 512"><path fill="currentColor" d="M224.3 273l-136 136c-9.4 9.4-24.6 9.4-33.9 0l-22.6-22.6c-9.4-9.4-9.4-24.6 0-33.9l96.4-96.4-96.4-96.4c-9.4-9.4-9.4-24.6 0-33.9L54.3 103c9.4-9.4 24.6-9.4 33.9 0l136 136c9.5 9.4 9.5 24.6.1 34z"></path></svg>
-            <span>Frete incluso para compra de Gerador Fotovoltaico (Kit) acima de 1,6kW. Não incluso descarga dos equipamentos para sistemas com potência acima de 6kW.</span>
+            <span>Frete a combinar.</span>
         </div>
     </div>
 </section>
