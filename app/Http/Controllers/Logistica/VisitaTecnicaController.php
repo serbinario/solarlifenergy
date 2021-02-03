7edeced05456c186df69e58615691aa756dd8916
Paulo Vaz <psgvaz@gmail.com>
2021-02-03 17:31:09 -0300

    Visita técnica

--- app/Http/Controllers/Logistica/VisitaTecnicaController.php
+++ app/Http/Controllers/Logistica/VisitaTecnicaController.php
@@ -178,7 +178,7 @@ class VisitaTecnicaController extends Controller
             $data['localizacao_image'] = $this->ImageStore($request, 'localizacao_image', $visitaTecnica->localizacao_image);
             $data['disjuntor_geral_image'] = $this->ImageStore($request, 'disjuntor_geral_image', $visitaTecnica->disjuntor_geral_image);
             $data['comprovante_image'] = $this->ImageStore($request, 'comprovante_image', $visitaTecnica->comprovante_image);
-                        
+
             $visitaTecnica->update($data);
             return redirect()->route('visita_tecnica.edit', $visitaTecnica->id)
                 ->with('success_message', 'Visita técnica atualizada com sucesso!');
