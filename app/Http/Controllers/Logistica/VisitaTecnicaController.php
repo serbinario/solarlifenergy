d58d01347a8fd9cd46c89ae46940c77862c58146
Paulo Vaz <psgvaz@gmail.com>
2021-02-15 15:42:27 -0300

    Visita técnica - select só aparece os técnicos

--- app/Http/Controllers/Logistica/VisitaTecnicaController.php
+++ app/Http/Controllers/Logistica/VisitaTecnicaController.php
@@ -223,7 +223,7 @@ class VisitaTecnicaController extends Controller
     {
         $visitaTecnica = VisitasTecnicas::with('projeto')->findOrFail($id);
         $status = StatusVisita::pluck('descricao','id')->all();
-        $users = User::orderBy('name')->pluck('name','id')->all();
+        $users = User::where('is_tecnico', '=','1')->orderBy('name')->pluck('name','id')->all();
         //dd($visitaTecnica);
         return view('logistica.visita_tecnica.edit', compact( 'visitaTecnica', 'status', 'users'));
 
